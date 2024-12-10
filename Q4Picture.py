35cd36826d7355f472f8ea4d927cf315211b5b05
Kamto Oguanobi <kamto.oguanobi@stu.ocsb.ca>
2024-12-10 08:32:11 -0500

    done

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,4 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+
+https://replit.com/@KamtoOguanobi/FrizzyNoteworthyArraylist
